c8e9a83d1cd8d0aa9ead14d1e378c63e56c711c7
Cleber96 <143430109+Cleber96@users.noreply.github.com>
2025-07-15 17:38:43 -0500

    Update config.py

--- nuevo_data/config.py
+++ nuevo_data/config.py
@@ -2,7 +2,7 @@
 import google.generativeai as genai
 
 # Aquí puedes cargar la clave API de una manera más segura, como desde una variable de entorno.
-API_KEY_GEMINI = 'AIzaSyAupffvXcdU2kS6oOd2YekGUIHJVdER0bk'
+API_KEY_GEMINI = 'AIzaSy__k'
 genai.configure(api_key=API_KEY_GEMINI)
 
 # Inicializa el modelo para reutilizarlo más tarde.
